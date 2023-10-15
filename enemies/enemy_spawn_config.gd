11a2e2285569ea61ed4c762cf5dbc0917d303792
Kooper Smith <kooper.lewis.smith@gmail.com>
2023-10-15 17:52:29 -0400

    reorganized

---
+++ enemies/enemy_spawn_config.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name EnemySpawnConfig
+
+@export var scene: PackedScene;
+@export var spawn_priority: int = 1;
---
+++ player/bullet.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+class_name Bullet
+
+@export var speed: int = 600;
+
+@onready var screen_checker := $VisibleOnScreenNotifier2D; 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_checker.screen_exited.connect(on_screen_exit)
+
+func on_screen_exit():
+	queue_free() # destroy self
+
+func _physics_process(delta):
+	translate(Vector2.UP * (speed * delta))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
